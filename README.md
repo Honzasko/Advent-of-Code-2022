ffa916d8d1ce7c18b1bb7a64a026534702487f0b
Honzasko <straka.jan5@seznam.cz>
2022-12-01 18:09:49 +0100

    Day 1

---
+++ README.md
@@ -0,0 +1,2 @@
+# Advent-of-Code-2022
+Minimum of Net 6.0 is required to compile and run this
---
+++ day1-part1.cs
@@ -0,0 +1,33 @@
+﻿
+String[] lines = File.ReadAllLines("input.txt");
+List<UInt64> calories = new List<UInt64>();
+UInt64 kcal = 0;
+foreach (string line in lines)
+{
+    if(line == "")
+    {
+        calories.Add(kcal);
+        kcal = 0;
+    }
+    else {
+        UInt64 k = UInt64.Parse(line);
+        kcal += k;
+    }
+}
+
+UInt64 max = 0;
+
+
+for(int i = 0;i < calories.Count;i++)
+{
+    if(calories[i] > max)
+    {
+
+        max = calories[i];
+    }
+
+}
+
+Console.WriteLine(max);
+
+
